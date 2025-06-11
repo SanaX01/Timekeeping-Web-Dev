60389c3b489bae2af0b0354dc2e0a8f1bfbd9941
Sana <christianjade.cjt@gmail.com>
2025-06-12 01:01:20 +0800

    feat: display current date in Home component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,10 +3,12 @@ import Statistics from "../_components/Statistics";
 import Welcome from "../_components/Welcome";
 
 export default function Home() {
+  const date = new Date();
   return (
     <main className="w-full container flex mx-auto justify-center items-center flex-col gap-y-10 flex-1 text-foreground relative my-24">
       <Statistics />
       <Welcome />
+      <div>{date.toString()}</div>
     </main>
   );
 }
